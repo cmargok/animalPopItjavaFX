8f79a5f46fbffdb6ff14662496d01364a52b7b02
cmargok <alphackode@gmail.com>
2022-04-26 17:22:41 -0500

    whops

---
+++ AnimalPopit/run.bat
@@ -0,0 +1,3 @@
+cd %userprofile%/desktop/AnimalPopit/Game
+
+java --module-path "%userprofile%/Desktop\AnimalPopit\Game\javafx\lib" --add-modules javafx.controls,javafx.fxml,javafx.media -jar "game.jar" 
